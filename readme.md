0d5971c27cb50fc40c3f8700e02ffc68bfe0061d
alexcastrodev <alexoliveira7x@gmail.com>
2026-02-16 22:12:30 +0000

    register hotkey and add readme

---
+++ readme.md
@@ -0,0 +1,10 @@
+# ScreenGif
+
+# Select area
+```
+CMD + Shift + 6
+```
+
+Start record
+
+![work](./work.gif)
--- screengif/Services/HotkeyManager.swift
+++ screengif/Services/HotkeyManager.swift
@@ -1,57 +1,92 @@
 import Cocoa
+import Carbon
 import os
 
-@MainActor
 final class HotkeyManager {
 
-    private var globalMonitor: Any?
-    private var localMonitor: Any?
+    private var hotKeyRef: EventHotKeyRef?
+    private var eventHandler: EventHandlerRef?
+    private var onTrigger: (() -> Void)?
     private let logger = Logger(subsystem: "com.lekito.screengif", category: "HotkeyManager")
 
-    func register(handler: @escaping @MainActor () -> Void) {
-        let targetKeyCode: UInt16 = 22
-        let targetModifiers: NSEvent.ModifierFlags = [.command, .shift]
+    init() {}
 
-        func matches(_ event: NSEvent) -> Bool {
-            let flags = event.modifierFlags.intersection(.deviceIndependentFlagsMask)
-            return event.keyCode == targetKeyCode && flags == targetModifiers
+    func register(handler: @escaping () -> Void) {
+        unregister()
+        self.onTrigger = handler
+
+        // ⌘⇧6
+        // Virtual Key Code for '6' is 0x16 (22)
+        let keyCode: UInt32 = 0x16
+        
+        // Modifiers: cmdKey | shiftKey
+        let modifiers = UInt32(cmdKey | shiftKey)
+
+        let signature = OSType(0x53474946) // "SGIF"
+        let hotKeyID = EventHotKeyID(signature: signature, id: 1)
+        
+        var ref: EventHotKeyRef?
+        let status = RegisterEventHotKey(
+            keyCode,
+            modifiers,
+            hotKeyID,
+            GetApplicationEventTarget(),
+            0,
+            &ref
+        )
+        
+        if status != noErr {
+            logger.error("Failed to register global hotkey: \(status)")
+            return
         }
+        self.hotKeyRef = ref
 
-        globalMonitor = NSEvent.addGlobalMonitorForEvents(matching: .keyDown) { event in
-            if matches(event) {
-                DispatchQueue.main.async { handler() }
-            }
-        }
-
-        localMonitor = NSEvent.addLocalMonitorForEvents(matching: .keyDown) { event in
-            if matches(event) {
-                DispatchQueue.main.async { handler() }
-                return nil
-            }
-            return event
-        }
-
-        logger.info("Hotkey registered: ⌘⇧6")
+        // Install Event Handler
+        var eventSpec = EventTypeSpec(eventClass: OSType(kEventClassKeyboard), eventKind: UInt32(kEventHotKeyPressed))
+        
+        let selfPtr = Unmanaged.passUnretained(self).toOpaque()
+        
+        InstallEventHandler(
+            GetApplicationEventTarget(),
+            GlobalHotkeyHandler,
+            1,
+            &eventSpec,
+            selfPtr,
+            &eventHandler
+        )
+        
+        logger.info("Global hotkey registered using Carbon: ⌘⇧6")
     }
 
     func unregister() {
-        if let globalMonitor {
-            NSEvent.removeMonitor(globalMonitor)
-            self.globalMonitor = nil
+        if let hotKeyRef = hotKeyRef {
+            UnregisterEventHotKey(hotKeyRef)
+            self.hotKeyRef = nil
         }
-        if let localMonitor {
-            NSEvent.removeMonitor(localMonitor)
-            self.localMonitor = nil
+        if let eventHandler = eventHandler {
+            RemoveEventHandler(eventHandler)
+            self.eventHandler = nil
         }
-        logger.info("Hotkey unregistered")
     }
-
+    
+    fileprivate func trigger() {
+        onTrigger?()
+    }
+    
     deinit {
-        if let globalMonitor {
-            NSEvent.removeMonitor(globalMonitor)
-        }
-        if let localMonitor {
-            NSEvent.removeMonitor(localMonitor)
-        }
+        unregister()
     }
+}
+
+// Global C-function callback for Carbon events
+private func GlobalHotkeyHandler(_ nextHandler: EventHandlerCallRef?, _ theEvent: EventRef?, _ userData: UnsafeMutableRawPointer?) -> OSStatus {
+    guard let userData = userData else { return noErr }
+    
+    let manager = Unmanaged<HotkeyManager>.fromOpaque(userData).takeUnretainedValue()
+    
+    Task { @MainActor in
+        manager.trigger()
+    }
+    
+    return noErr
 }
