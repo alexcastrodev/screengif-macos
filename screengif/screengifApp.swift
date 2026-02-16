fe72271f065137accec651af5e16a3c42b9e7426
alexcastrodev <alexoliveira7x@gmail.com>
2026-02-16 16:29:43 +0000

    Initial Commit

---
+++ screengif/screengifApp.swift
@@ -0,0 +1,17 @@
+//
+//  screengifApp.swift
+//  screengif
+//
+//  Created by Alexandro castro on 16/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct screengifApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
